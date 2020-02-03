2c6c241a6f81c321967572a3a10edf16743a743f
KushwahAshishSingh <kushwah007.as@gmail.com>
2020-02-03 16:01:15 +0530

    database connection mongo atlus

---
+++ app.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoConnect = require('./util/database').mongoConnect;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req,res,next)=> { 
+    next();
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+
+mongoConnect ( () => {
+    app.listen(3000);
+});
---
+++ ignore.js
@@ -0,0 +1,34 @@
+// const bodyParser = require('body-parser');
+
+// const express = require('express');
+// const path = require('path');
+
+// const expressHBS = require('express-handlebars');
+// const app = express();
+
+// app.engine('hbs',expressHBS({
+//     layoutsDir: 'views/layouts/',
+//     defaultLayout: 'main-layout',
+//     extname: 'hbs'
+//     })
+// );
+// app.set('view engine', 'hbs');
+// app.set('views','views');
+
+// const adminRoutes = require('./routes/admin');
+// const routesShop = require('./routes/shop');
+// const error404 = require('./controllers/shop');
+
+
+// app.use(bodyParser.urlencoded({extended: false}));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use(adminRoutes);
+// app.use(routesShop);
+// app.use(error404);
+
+
+
+
+
+// app.listen(3000);
